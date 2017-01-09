fb97a13d99b62b5dea8f7456f4d7dc30b87a885e
sing1ee <zh.milo@gmail.com>
2017-01-09 21:01:05 +0800

    complete ii_collections

--- src/ii_collections/_15_AllAnyAndOtherPredicates.kt
+++ src/ii_collections/_15_AllAnyAndOtherPredicates.kt
@@ -15,25 +15,29 @@ fun example2(list: List<Int>) {
 
 fun Customer.isFrom(city: City): Boolean {
     // Return true if the customer is from the given city
-    todoCollectionTask()
+    return this.city.name.equals(city.name)
 }
 
 fun Shop.checkAllCustomersAreFrom(city: City): Boolean {
     // Return true if all customers are from the given city
-    todoCollectionTask()
+    return this.customers.filter { it.isFrom(city) }.size == this.customers.size
 }
 
 fun Shop.hasCustomerFrom(city: City): Boolean {
     // Return true if there is at least one customer from the given city
-    todoCollectionTask()
+    return this.customers.filter { it.isFrom(city) }.size > 0
 }
 
 fun Shop.countCustomersFrom(city: City): Int {
     // Return the number of customers from the given city
-    todoCollectionTask()
+    return this.customers.filter { it.isFrom(city) }.size
 }
 
 fun Shop.findAnyCustomerFrom(city: City): Customer? {
     // Return a customer who lives in the given city, or null if there is none
-    todoCollectionTask()
+    val cs = this.customers.filter { it.isFrom(city) }
+    if (cs.size > 0) {
+        return cs[0]
+    }
+    return null
 }
